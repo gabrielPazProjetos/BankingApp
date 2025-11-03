3709e0afff6d0dc3574f19240a8ceab02f732430
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-11-03 19:34:21 -0300

    Update BankAccount.cs

--- Classes_M3/BankAccount.cs
+++ Classes_M3/BankAccount.cs
@@ -1,1 +1,98 @@
+using System;
 
+namespace Classes_M3;
+
+public class BankAccount
+{
+    private static int s_nextAccountNumber;
+    public static double InterestRate { get; private set; }
+    public static double TransactionRate { get; private set; }
+    public static double MaxTransactionFee { get; private set; }
+    public static double OverdraftRate { get; private set; }
+    public static double MaxOverdraftFee { get; private set; }
+
+    public int AccountNumber { get; }
+    public string CustomerId { get; }
+    public double Balance { get; private set; } = 0;
+    public string AccountType { get; set; } = "Checking";
+
+    static BankAccount()
+    {
+        Random random = new Random();
+        s_nextAccountNumber = random.Next(10000000, 20000000);
+        InterestRate = 0.00;
+        TransactionRate = 0.01;
+        MaxTransactionFee = 10;
+        OverdraftRate = 0.05;
+        MaxOverdraftFee = 10;
+    }
+
+    public BankAccount(string customerIdNumber, double balance = 200, string accountType = "Checking")
+    {
+        this.AccountNumber = s_nextAccountNumber++;
+        this.CustomerId = customerIdNumber;
+        this.Balance = balance;
+        this.AccountType = accountType;
+    }
+
+    // Copy constructor for BankAccount
+    public BankAccount(BankAccount existingAccount)
+    {
+        this.AccountNumber = s_nextAccountNumber++;
+        this.CustomerId = existingAccount.CustomerId;
+        this.Balance = existingAccount.Balance;
+        this.AccountType = existingAccount.AccountType;
+    }
+
+    public void Deposit(double amount)
+    {
+        Balance += amount;
+    }
+
+    public bool Withdraw(double amount)
+    {
+        if (amount > 0 && Balance >= amount)
+        {
+            Balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public bool Transfer(BankAccount destinationAccount, double amount)
+    {
+        if (amount > 0 && Balance >= amount)
+        {
+            Balance -= amount;
+            destinationAccount.Deposit(amount);
+            return true;
+        }
+        return false;
+    }
+
+    public void ApplyInterest(double years)
+    {
+        Balance += AccountCalculations.CalculateCompoundInterest(Balance, InterestRate, years);
+    }
+
+    public bool IssueCashiersCheck(double amount)
+    {
+        if (amount > 0 && Balance >= amount + MaxTransactionFee)
+        {
+            Balance -= amount;
+            Balance -= AccountCalculations.CalculateTransactionFee(amount, TransactionRate, MaxTransactionFee);
+            return true;
+        }
+        return false;
+    }
+
+    public void ApplyRefund(double refund)
+    {
+        Balance += refund;
+    }
+
+    public string DisplayAccountInfo()
+    {
+        return $"Account Number: {AccountNumber}, Type: {AccountType}, Balance: {Balance}, Interest Rate: {InterestRate}, Customer ID: {CustomerId}";
+    }
+}
