67b5d653abe657b79aa2e22326b95bb6cdbc3ddb
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-11-03 19:33:49 -0300

    Update BankCustomerMethods.cs

--- Classes_M3/BankCustomerMethods.cs
+++ Classes_M3/BankCustomerMethods.cs
@@ -1,1 +1,22 @@
+using System;
 
+namespace Classes_M3;
+
+public partial class BankCustomer
+{
+    public string ReturnFullName()
+    {
+        return $"{FirstName} {LastName}";
+    }
+
+    public void UpdateName(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public string DisplayCustomerInfo()
+    {
+        return $"Customer ID: {CustomerId}, Name: {ReturnFullName()}";
+    }
+}
