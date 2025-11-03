5a53746ac6663113d6491e386b1fab4fd77e96ee
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-11-03 19:36:01 -0300

    Update Extensions.cs

--- Classes_M3/Extensions.cs
+++ Classes_M3/Extensions.cs
@@ -1,1 +1,29 @@
+using System;
 
+namespace Classes_M3;
+
+public static class BankCustomerExtensions
+{
+    public static bool IsValidCustomerId(this BankCustomer customer)
+    {
+        return customer.CustomerId.Length == 10;
+    }
+
+    public static string GreetCustomer(this BankCustomer customer)
+    {
+        return $"Hello, {customer.ReturnFullName()}!";
+    }
+}
+
+public static class BankAccountExtensions
+{
+    public static bool IsOverdrawn(this BankAccount account)
+    {
+        return account.Balance < 0;
+    }
+
+    public static bool CanWithdraw(this BankAccount account, double amount)
+    {
+        return account.Balance >= amount;
+    }
+}
